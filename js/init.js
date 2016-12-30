6e7d8ff8a81365c7ecd8e1762beaf6893ed32199
aledelgado94 <alejandrodelgadomartel@gmail.com>
2016-12-30 20:01:59 +0000

    page in construction

--- js/init.js
+++ js/init.js
@@ -1,10 +1,20 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
     $('.materialboxed').materialbox();
     $('.collapsible').collapsible();
+    $('.modal').modal();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+$(document).ready(function(){
+      $('.button-collapse').sideNav();
+    /*  $('.side-nav').click(function() {
+        $('.nav-wrapper').display="none";
+      });*/
+  });
